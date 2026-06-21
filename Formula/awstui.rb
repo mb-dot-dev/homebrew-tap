d2243dabf6c9cbcbd9287f86ea7422def4dbbac1
Bence Molnár <molnarbence@users.noreply.github.com>
2026-06-21 21:24:50 +0200

    awstui 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/awstui.rb
+++ Formula/awstui.rb
@@ -1,9 +1,9 @@
 class Awstui < Formula
   desc "Terminal UI for the AWS Console"
   homepage "https://github.com/mb-dot-dev/aws-console-tui"
-  version "0.0.0"
-  url "https://github.com/mb-dot-dev/aws-console-tui/releases/download/v0.0.0/awstui-0.0.0-osx-arm64.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.1.0"
+  url "https://github.com/mb-dot-dev/aws-console-tui/releases/download/v0.1.0/awstui-0.1.0-osx-arm64.tar.gz"
+  sha256 "43a1eb6f50c9d6e8a336e7bbeadbc245224ecf13d9a2e587aa2477c3e5dd2cb8"
   depends_on macos: :big_sur
   depends_on arch: :arm64
 
